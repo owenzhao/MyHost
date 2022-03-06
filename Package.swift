ab84b5dc89ec60cccfb9c6b973fda010a8c6aa11
肇鑫（Zhao Xin） <owenzx@gmail.com>
2022-03-06 20:42:49 +0800

    * update framework with my own fork.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:master-nevi/Reachability.git", Package.Dependency.Requirement.branch("master")),
+        .package(url: "git@github.com:owenzhao/Reachability.git", Package.Dependency.Requirement.branch("3.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
