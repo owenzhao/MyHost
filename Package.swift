6c76e501f811753c26755827ca3056a55af25501
肇鑫（Zhao Xin） <owenzx@gmail.com>
2022-04-10 14:46:04 +0800

    * updated build settings.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url:"git@github.com:sindresorhus/Defaults.git", from: "6.2.0"),
-        .package(url:"git@github.com:owenzhao/SpeedTestServiceNotification.git", from: "1.0.0"),
+        .package(url:"git@github.com:owenzhao/SpeedTestServiceNotification.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
